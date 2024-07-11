173409b3ff52e15ad06a4884b4a3ca5b84bf33e9
RASUL YEA BARI SUNBEAM <sunbeam20@outlook.com>
2024-07-11 22:04:51 +0800

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`http://localhost:5555/books`)
+      .get(`https://book-store-mern-chi.vercel.app/books`)
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
