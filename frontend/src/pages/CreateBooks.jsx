89c303f3b267e3ee9f9abe0cfa3d1cc2c47cfad5
RASUL YEA BARI SUNBEAM <sunbeam20@outlook.com>
2024-07-11 22:04:00 +0800

    Update CreateBooks.jsx

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -21,7 +21,7 @@ const CreateBooks = () => {
     };
     setLoading(true);
     axios
-      .post('http://localhost:5555/books', data)
+      .post('https://book-store-mern-chi.vercel.app/books', data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Created Succesfully', { variant: 'success' });
