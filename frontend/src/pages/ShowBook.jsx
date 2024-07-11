f5d07d5e66ade9367c22f8ccef2c635ec9449c36
RASUL YEA BARI SUNBEAM <sunbeam20@outlook.com>
2024-07-11 22:05:16 +0800

    Update ShowBook.jsx

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -12,7 +12,7 @@ const ShowBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`http://localhost:5555/books/${id}`)
+      .get(`https://book-store-mern-chi.vercel.app/books/${id}`)
       .then((response) => {
         setBook(response.data);
         setLoading(false);
