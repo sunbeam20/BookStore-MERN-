bcfe20aaebec7aa51b0803e89bd08f77cc524694
RASUL YEA BARI SUNBEAM <sunbeam20@outlook.com>
2024-07-11 23:17:13 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 // app.use(cors());
 // 2) Allow custom origins
 app.use(cors({
-    origin: 'https://book-store-mern-frontend-seven.vercel.app',  // Replace with your frontend's URL
+    origin: '*',  // Replace with your frontend's URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allowed HTTP methods
     credentials: true  // Allow cookies for authenticated requests (if applicable)
 }));
